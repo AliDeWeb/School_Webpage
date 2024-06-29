e8f771344cd5675e7b8f87aea3e36c5bc423c651
Alideweb <alimoradi0business@gmail.com>
2024-06-29 11:12:02 -0700

    jwt token is available now

--- backend/controllers/AuthenticationController/SignupController.ts
+++ backend/controllers/AuthenticationController/SignupController.ts
@@ -1,6 +1,9 @@
 import { NextFunction, Request, Response } from "express";
 import UserModel from "../../models/UserModel/UserModel";
 import catchAsync from "../../utils/CatchAsync/CatchAsync";
+import jwt from "jsonwebtoken";
+import userModel from "../../models/UserModel/UserModel";
+import AppError from "../../utils/AppError/AppError";
 
 export const signup = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -20,7 +23,11 @@ export const signup = catchAsync(
       password: string;
     } = req.body;
 
-    await UserModel.create({
+    const isAnyUserExistUser = await userModel.findOne({ phoneNumber });
+    if ((isAnyUserExistUser as { phoneNumber: string })?.phoneNumber)
+      next(new AppError("شما با این شماره تلفن حساب کاربری دارید", 401));
+
+    const newUser = await UserModel.create({
       name,
       lastName,
       phoneNumber,
@@ -29,8 +36,17 @@ export const signup = catchAsync(
       password,
     });
 
+    const token = jwt.sign(
+      { id: newUser._id },
+      process.env.JWT_SECRET as string,
+      {
+        expiresIn: process.env.JWT_EXPIRES_IN as string,
+      },
+    );
+
     res.status(201).json({
       message: "خوش آمدید",
+      token,
       data: {
         name,
         lastName,
