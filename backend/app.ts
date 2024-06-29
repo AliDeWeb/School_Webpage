8386fbf4e7b56c6616c05f6f84b49a427e944e6b
Alideweb <alimoradi0business@gmail.com>
2024-06-29 09:38:47 -0700

    simple signup api completed

--- backend/app.ts
+++ backend/app.ts
@@ -1,10 +1,14 @@
 import express, { NextFunction, Request, Response } from "express";
+import signupRouter from "./routes/AuthenticationRoutes/AuthenticationRoutes";
 import errorController from "./controllers/ErrorController/ErrorController";
 import AppError from "./utils/AppError/AppError";
 
 const app = express();
 
 // Middlewares
+app.use(express.json());
+// <-- Authentication -->
+app.use("/api/v1/auth", signupRouter);
 // <-- 404 Route -->
 app.use("*", (req: Request, res: Response, next: NextFunction) => {
   next(new AppError(`Route is not defined`, 404));
---
+++ backend/controllers/AuthenticationController/SignupController.ts
@@ -0,0 +1,44 @@
+import { NextFunction, Request, Response } from "express";
+import UserModel from "../../models/UserModel/UserModel";
+
+export const signup = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const {
+    name,
+    lastName,
+    phoneNumber,
+    classNumber,
+    birthday,
+    password,
+  }: {
+    name: string;
+    lastName: string;
+    phoneNumber: string;
+    classNumber: string;
+    birthday: string;
+    password: string;
+  } = req.body;
+
+  await UserModel.create({
+    name,
+    lastName,
+    phoneNumber,
+    classNumber,
+    birthday,
+    password,
+  });
+
+  res.status(201).json({
+    message: "خوش آمدید",
+    data: {
+      name,
+      lastName,
+      phoneNumber,
+      classNumber,
+      birthday,
+    },
+  });
+};
