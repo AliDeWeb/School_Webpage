5d1a23a44283d453abb60276624f83545d22d891
Alideweb <alimoradi0business@gmail.com>
2024-06-30 05:17:58 -0700

    some pkgs added for more security

--- backend/app.ts
+++ backend/app.ts
@@ -3,11 +3,55 @@ import signupRouter from "./routes/AuthenticationRoutes/AuthenticationRoutes";
 import errorController from "./controllers/ErrorController/ErrorController";
 import AppError from "./utils/AppError/AppError";
 import { errors } from "celebrate";
+import cors from "cors";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
+import hpp from "hpp";
+import mongoSanitize from "express-mongo-sanitize";
+// @ts-ignore
+import xss from "xss-clean";
 
+// Logics
 const app = express();
+// <-- express-rate-limit -->
+const limiter = rateLimit({
+  message: "درخواست بیش از حد!",
+  max: 300,
+  windowMs:
+    (Number(process.env.MAX_REQUESTS_LIMIT_TIME as string) || 1) *
+    60 *
+    60 *
+    1000,
+});
+// <-- Cors -->
+const whitelist = [`http://127.0.0.1:${process.env.PORT || 3000}`];
+const corsOptions = {
+  origin: function (origin: any, callback: any) {
+    if (!origin) {
+      return callback(null, true);
+    }
+
+    if (whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("دسترسی رد شد"), false);
+    }
+  },
+};
 
 // Middlewares
-app.use(express.json());
+app.use(cors(corsOptions));
+// <-- Security -->
+app.use(helmet());
+// <-- Limitation -->
+app.use("/api", limiter);
+// <-- Body Parser -->
+app.use(express.json({ limit: "10kb" }));
+// <-- Data Sanitizing -->
+app.use(mongoSanitize());
+app.use(xss());
+// <-- Preventing Params Pollution -->
+app.use(hpp());
 // <-- Authentication -->
 app.use("/api/v1/auth", signupRouter);
 // <-- 404 Route -->
