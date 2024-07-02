4ad2f121ec30c8b8cfa774b9d5b80def2bcdf200
Alideweb <alimoradi0business@gmail.com>
2024-07-02 09:49:56 -0700

    react query devtools added

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,6 +8,7 @@ import frFR from "antd/locale/fr_FR";
 
 // React Query
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
@@ -17,6 +18,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <ConfigProvider locale={frFR}>
         <App />
       </ConfigProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>,
 );
