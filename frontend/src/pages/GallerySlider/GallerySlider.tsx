e5fa9cebda0f792e73a5cecf203b2115b2ece6c2
Alideweb <alimoradi0business@gmail.com>
2024-07-03 13:11:48 -0700

    gallery page redesigned

--- frontend/src/pages/GallerySlider/GallerySlider.tsx
+++ frontend/src/pages/GallerySlider/GallerySlider.tsx
@@ -1,78 +1,77 @@
+import { useEffect } from "react";
+
 // SwiperJs
-import { Navigation, Scrollbar, A11y } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
 import "swiper/css";
-import "swiper/css/navigation";
 import "swiper/css/pagination";
-import "swiper/css/scrollbar";
+
+// React Query
+import { useQuery } from "@tanstack/react-query";
+
+// Axios
+import { baseUrl, gallery } from "../../configs/axios";
+
+// Ant Design
+import { notification } from "antd";
 
 const GallerySlider = () => {
+  useEffect(() => {
+    notification.info({
+      duration: 4,
+      message: "راهنما",
+      description: "با موس بر روی تصاویر کلیک کرده و به بالا یا پایین بکشید.",
+    });
+  }, []);
+
+  const { data: galleryImgs, isLoading: isGalleryImgsLoading } = useQuery({
+    queryKey: ["GalleryImgs?limit=80"],
+    queryFn: async () => {
+      const galleryData = await gallery.get("/?limit=80", {
+        method: "GET",
+      });
+
+      return galleryData?.data;
+    },
+    refetchInterval: 60 * 60 * 1000,
+    refetchOnReconnect: true,
+    refetchIntervalInBackground: true,
+    refetchOnMount: true,
+    refetchOnWindowFocus: true,
+    staleTime: 60 * 60 * 1000,
+  });
+
   return (
-    <div
+    <Swiper
+      direction={"vertical"}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[Pagination]}
       className={
-        "h-[100dvh] w-full bg-black -mt-20 flex items-center justify-center hover:cursor-grab active:cursor-grabbing"
+        "w-screen h-[100dvh] -mt-20 flex items-center justify-center bg-black"
       }
     >
-      <div className="container">
-        <Swiper
-          modules={[Navigation, Scrollbar, A11y]}
-          spaceBetween={50}
-          slidesPerView={1}
-          navigation
-          scrollbar={{ draggable: true }}
-        >
-          <SwiperSlide>
+      {!isGalleryImgsLoading &&
+        !!galleryImgs?.data?.gallery?.length &&
+        galleryImgs.data.gallery.map((el: any) => (
+          <SwiperSlide key={el._id}>
             <div
               className={
-                "w-[80%] mx-auto flex items-center overflow-hidden rounded-2xl shadow-lg shadow-blue-200/30 my-6"
+                "w-screen h-[100dvh] flex items-center justify-center text-white hover:cursor-grab active:cursor-grabbing"
               }
             >
-              <img
-                src="https://engage-education.com/wp-content/uploads/2022/08/Private-School.jpg"
-                alt=""
-              />
+              <div className={"h-[90dvh] w-[80%] relative"}>
+                <img
+                  src={`${baseUrl}/${el.image}`}
+                  alt="img"
+                  className={"object-contain overflow-hidden rounded-2xl"}
+                />
+              </div>
             </div>
           </SwiperSlide>
-          <SwiperSlide>
-            <div
-              className={
-                "w-[80%] mx-auto flex items-center overflow-hidden rounded-2xl shadow-lg shadow-blue-200/30 my-6"
-              }
-            >
-              <img
-                src="https://engage-education.com/wp-content/uploads/2022/08/Private-School.jpg"
-                alt=""
-              />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div
-              className={
-                "w-[80%] mx-auto flex items-center overflow-hidden rounded-2xl shadow-lg shadow-blue-200/30 my-6"
-              }
-            >
-              <img
-                src="https://engage-education.com/wp-content/uploads/2022/08/Private-School.jpg"
-                alt="img"
-                className={"object-cover"}
-              />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div
-              className={
-                "w-[80%] max-h-[600px] mx-auto flex items-center overflow-hidden rounded-2xl shadow-lg shadow-blue-200/30 my-6"
-              }
-            >
-              <img
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Tour_Eiffel_Wikimedia_Commons_%28cropped%29.jpg/800px-Tour_Eiffel_Wikimedia_Commons_%28cropped%29.jpg"
-                alt=""
-              />
-            </div>
-          </SwiperSlide>
-        </Swiper>
-      </div>
-    </div>
+        ))}
+    </Swiper>
   );
 };
 
